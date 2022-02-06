72452c7e751c6abf5152738e68d4a8e1314533dd
Lateef Azeez <codelate18@gmail.com>
2022-02-05 20:01:10 -0700

    create deploy script and deploy locally

--- contracts/WebPortal.sol
+++ contracts/WebPortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WebPortal {
+  uint256 totalWaves;
+
   constructor() {
-    console.log("Yo yo, I am a contract and I am smart");
+    console.log("Yippeee!!! I just ran my first smart contract on blockchain");
+  }
+
+  function wave() public {
+    totalWaves += 1;
+    console.log("%s has waved!", msg.sender);
+  }
+
+  function getTotalWaves() public view returns (uint256) {
+    console.log("We have %d total waves!", totalWaves);
+    return totalWaves;
   }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,36 @@
 const main = async () => {
+  const [owner, buyerOne, buyerTwo] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WebPortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
+
+  let buyerOneWaves = 0;
+  let buyerTwoWaves = 0;
+
   console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  for (let i = 0; i < 10; i++) {
+    waveTxn = await waveContract.connect(buyerOne).wave();
+    buyerOneWaves += 1;
+    await waveTxn.wait();
+  }
+
+  waveTxn = await waveContract.connect(buyerTwo).wave();
+  buyerTwoWaves += 1;
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+  console.log("buyerOne waved %s times!", buyerOneWaves);
+  console.log("buyerTwo waved %s times!", buyerTwoWaves);
 };
 
 const runMain = async () => {
