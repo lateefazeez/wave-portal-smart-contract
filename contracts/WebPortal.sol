6e17481df34f579d619ce59152c8199d68138600
Lateef Azeez <codelate18@gmail.com>
2022-02-12 21:17:40 -0700

    get live waves in real time

--- contracts/WebPortal.sol
+++ contracts/WebPortal.sol
@@ -7,6 +7,11 @@ import "hardhat/console.sol";
 contract WebPortal {
   uint256 totalWaves;
 
+  /*
+     * We will be using this below to help generate a random number
+     */
+  uint256 seed;
+
   event NewWave(address indexed from, uint256 timestamp, string message);
 
   struct Wave {
@@ -17,24 +22,53 @@ contract WebPortal {
 
   Wave[] waves;
 
+   /*
+     * This is an address => uint mapping, meaning I can associate an address with a number!
+     * In this case, I'll be storing the address with the last time the user waved at us.
+     */
+  mapping(address => uint256) public lastWavedAt;
+
   constructor() payable {
     console.log("We have been constructed!");
+    /*
+    * Set the initial seed
+    */
+    seed = (block.timestamp + block.difficulty) % 100;
   }
 
   function wave(string memory _message) public {
+
+    require(lastWavedAt[msg.sender] + 30 seconds < block.timestamp, "Must wait 30 seconds before waving again.");
+
+    lastWavedAt[msg.sender] = block.timestamp;
+
     totalWaves += 1;
     console.log("%s has waved", msg.sender);
 
     waves.push(Wave(msg.sender, _message, block.timestamp));
+    /*
+    * Generate a new seed for the next user that sends a wave
+    */
+    seed = (block.difficulty + block.timestamp + seed) % 100;
+
+    console.log("Random # generated: %d", seed);
+
+    /*
+    * Give a 50% chance that the user wins the prize.
+    */
+    if (seed <= 50) {
+      console.log("%s won!", msg.sender);
+
+      uint256 prizeAmount = 0.0001 ether;
+      require(
+          prizeAmount <= address(this).balance,
+          "Trying to withdraw more money than the contract has."
+      );
+      (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+      require(success, "Failed to withdraw money from contract.");
+    }
+
     emit NewWave(msg.sender, block.timestamp, _message);
-
-    uint256 prizeAmount = 0.0001 ether;
-    require(
-      prizeAmount <= address(this).balance, "Trying to withdraw more money than the contract has"
-    );
-
-    (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-    require(success, "Failed to withdraw money from contract.");
   }
 
   function getAllWaves() public view returns (Wave[] memory) {
@@ -42,7 +76,6 @@ contract WebPortal {
   }
 
   function getTotalWaves() public view returns (uint256) {
-    console.log("We have %d total waves!", totalWaves);
     return totalWaves;
   }
 }
